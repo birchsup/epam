a9a33396c93e1f8985cf60f21822ec7dcf3f75e2
Dmitrii Sadovnikov <dmitrii.sadovnikov@planoly.com>
2023-04-03 00:12:25 +0400

    fic

--- tasks/module-2/variables.js
+++ tasks/module-2/variables.js
@@ -7,24 +7,24 @@
  * create variable named int and set value equal to 1
  */
 
-var int = 1;
+let int = 1;
 
 /**
  *  create variable named int2 and set value equal int plus 1
  */
-var int2 = int + 1;
+let int2 = int + 1;
 /**
  * create variable named str and set value equal to "John"
  */
-var str = "John";
+let str = "John";
 /**
  * create variable named str2 and set value equal str plus " Dou"
  */
-var str2 = str + " Dou";
+let str2 = str + " Dou";
 /**
  * create variable named bool and set value of equality comparison of int and int2 variables
  */
-var bool = int == int2;
+let bool = int === int2;
 /**
  * create variable named arr and set value array of numbers from 1 to 5
  */
@@ -38,17 +38,13 @@ let fifth = arr[4];
 /**
  * create variable named human and set value as object with key firstName and value "John"
  */
-// let human = { 
-//   firstName: "John"
-// };
-let human = {
-  firstName: "John",
-  lastName: "Dou"
-  };
+let human = { 
+  firstName: "John"
+};
 /**
  * set new key of human object named lastName and value "Dou"
  */
-// human.lastName = "Dou";
+human.lastName = "Dou";
 /**
  * set new key of human object named fullName and value of concatenation of firstName and lastName values
  */
